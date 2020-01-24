c4e6e3593e5c854f464370a859fcf499ab70f684
Aprendiz <team@platzi.com>
2020-01-24 10:14:28 -0600

    Selector de paises funcionando

--- src/app/interfaces/player.ts
+++ src/app/interfaces/player.ts
@@ -2,9 +2,47 @@ export class Player{
   key?: string;
   nombre  = 'Francisco';
   apellido = 'Palencia';
-  pais = 'CRI';
+  pais = Countries;
 }
 
+export const Countries = [
+  {
+    codigo: 'MX',
+    pai: 'Mexico'
+  },
+  {
+    codigo: 'COL',
+    pai: 'Colombia'
+  },
+  {
+    codigo: 'DZ',
+    pai: 'Algeria'
+  },
+  {
+    codigo: 'BE',
+    pai: 'Belgium'
+  },
+  {
+    codigo: 'BO',
+    pai: 'Bolivia'
+  },
+  {
+    codigo: 'HR',
+    pai: 'Croatia'
+  },
+  {
+    codigo: 'CH',
+    pai: 'Switzerland'
+  },
+  {
+    codigo: 'AR',
+    pai: 'Argentina',
+  }
+
+
+
+
+]
 
 
 
--- src/app/pages/jugador/jugador.component.ts
+++ src/app/pages/jugador/jugador.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {Player} from '../../interfaces/player';
+import {Player, Countries} from '../../interfaces/player';
 import {FormGroup, FormControl, Validators} from '@angular/forms';
 
 @Component({
@@ -10,11 +10,13 @@ import {FormGroup, FormControl, Validators} from '@angular/forms';
 export class JugadorComponent implements OnInit {
   forma: FormGroup;
   jugador: Player = new Player();
+  paises = Countries;
 
   constructor() {
     this.forma = new FormGroup({
       nombre: new FormControl(this.jugador.nombre, [Validators.required, Validators.minLength(2)]),
-      apellido: new FormControl(this.jugador.apellido, [Validators.required, Validators.minLength(2)])
+      apellido: new FormControl(this.jugador.apellido, [Validators.required, Validators.minLength(2)]),
+      pais: new FormControl(this.jugador.pais,[Validators.required])
     })
   }
 
@@ -23,6 +25,7 @@ export class JugadorComponent implements OnInit {
   guardar(){
     console.log(this.forma);
     console.log(this.jugador);
+    console.log(this.forma.value);
   }
 
 }
